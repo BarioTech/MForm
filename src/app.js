1b49b33a3a14e1175d28e361d3d23f6db9d92674
Mauro Soares <mauro@tempocerto.inf.br>
2025-01-22 18:49:33 -0300

    :art: no CORS test

--- src/app.js
+++ src/app.js
@@ -12,15 +12,15 @@ const userRoute = require('./routes/usuarios');
 const authRoute = require('./routes/auth');
 
 require('./mongodb');
-var cors = require('cors');
-app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// var cors = require('cors');
+// app.use(cors());
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 app.use('/', jsonParser, index);
 app.use('/usuarios', jsonParser, userRoute);
 app.use('/auth', jsonParser, authRoute);
